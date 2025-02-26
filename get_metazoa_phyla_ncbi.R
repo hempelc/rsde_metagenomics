1dfbb7b34ed9a622a69edaf05c538626f8f7af29
Christopher Hempel <chempel.work@gmail.com>
2025-02-26 09:43:26 -0500

    Removed absolute paths

--- get_metazoa_phyla_ncbi.R
+++ get_metazoa_phyla_ncbi.R
@@ -1,5 +1,5 @@
 devtools::install_github("sherrillmix/taxonomizr")
 library(taxonomizr)
-dbfile <- '/Users/simplexdna/Desktop/accessionTaxa.sql'
+dbfile <- 'accessionTaxa.sql'
 prepareDatabase(dbfile, getAccessions=FALSE)
 getDescendants(33208, sqlFile = dbfile, desiredTaxa = "phylum")
